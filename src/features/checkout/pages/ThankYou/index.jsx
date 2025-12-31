13af95b4472c30ff65d5780944e5afbb59ffbdbc
JAPASGAR <japasgar@gmail.com>
2025-12-31 14:37:35 +0100

    Product: añadir en thankyoupage el redireccionamiento

--- src/features/checkout/pages/ThankYou/index.jsx
+++ src/features/checkout/pages/ThankYou/index.jsx
@@ -1,3 +1,7 @@
+// Packages
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 // App
 import Button from 'shared/components/Button';
 import Container from 'shared/components/Container';
@@ -7,6 +11,24 @@ import ThanksImage from 'assets/images/thanks.svg';
 import './styles.scss';
 
 function ThankYouPage() {
+
+      const [countdown, setCountdown] = useState(5);
+    const navigate = useNavigate();
+
+      useEffect(() => {
+          const interval = setInterval(() => {
+              setCountdown(prev => prev - 1);
+          }, 1000);
+  
+          return () => clearInterval(interval);
+      }, []);
+  
+      useEffect(() => {
+          if (countdown === 0) {
+              navigate('/catalog');
+          }
+      }, [countdown, navigate]);
+
   return (
     <Container className="thank-you">
       <section className="thank-you__message">
@@ -16,9 +38,10 @@ function ThankYouPage() {
         </p>
 
         <div className="thank-you__actions">
-          <Button>Mis pedidos</Button>
+          {/* <Button>Mis pedidos</Button> */}
           <Button variant='outline' href="/catalog">Seguir comprando</Button>
         </div>
+        <p className='welcome__countdown'>Redirigiendo en {countdown} segundos...</p>
       </section>
 
       <section className="thank-you__image">
